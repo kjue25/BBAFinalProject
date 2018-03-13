93e1df4c93761b0ca8269daf2079227f459ac439
Kylie Jue <kjue25@gmail.com>
2018-03-13 01:39:01 -0700

    CLean up code

--- app/src/main/java/com/example/kyliejue/bbafinalproject/SensorActivity.java
+++ app/src/main/java/com/example/kyliejue/bbafinalproject/SensorActivity.java
@@ -7,18 +7,9 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.RelativeLayout;
-
-import static java.lang.Boolean.FALSE;
-import static java.lang.Boolean.TRUE;
 
 public class SensorActivity extends AppCompatActivity implements SensorEventListener {
     private SensorManager mSensorManager;
