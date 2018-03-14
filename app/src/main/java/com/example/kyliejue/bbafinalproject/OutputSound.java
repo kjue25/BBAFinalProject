74108ff463cf5b8a996801ccb5fc097063674217
Dan Melton <melton.dan@gmail.com>
2018-03-14 16:43:20 -0700

    added files for sound

---
+++ app/src/main/java/com/example/kyliejue/bbafinalproject/OutputSound.java
@@ -0,0 +1,16 @@
+package com.example.kyliejue.bbafinalproject;
+
+/**
+ * Created by aashnashroff on 3/14/18.
+ */
+
+public class OutputSound implements Output {
+    public OutputSound(){
+
+    }
+
+    @Override
+    public void onTrigger() {
+
+    }
+}
