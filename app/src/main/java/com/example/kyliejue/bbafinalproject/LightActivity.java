d6f118d8ff6dcf1750f9e1b800cf07d64bbcc36a
Kylie Jue <kjue25@gmail.com>
2018-03-15 01:36:16 -0700

    LightActivity has all the elements.  Need to connect text box to chosenVal.

--- app/src/main/java/com/example/kyliejue/bbafinalproject/LightActivity.java
+++ app/src/main/java/com/example/kyliejue/bbafinalproject/LightActivity.java
@@ -6,18 +6,42 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class LightActivity extends AppCompatActivity {
+public class LightActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
+    private String operand;
+    private String chosenVal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_light);
+
+        Spinner spinner = findViewById(R.id.operand_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.operand_spinner, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+        operand = parent.getItemAtPosition(pos).toString();
+        Spinner spinner = findViewById(R.id.operand_spinner);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        operand = "=";
     }
 
     public void setLightSensor(View view) {
         Intent intent = new Intent();
-        Condition lightCondition = new Condition(Sensor.TYPE_LIGHT, "=", "10");
+        Condition lightCondition = new Condition(Sensor.TYPE_LIGHT, operand, chosenVal);
         Log.d("STATE", "Created Condition with op: " + lightCondition.getOp());
         Log.d("STATE", "Created Condition with chosenVal: " + lightCondition.getChosenValue());
         Log.d("STATE", "Created Condition with sensor: "+ Double.toString(lightCondition.getSensor()));
--- app/src/main/java/com/example/kyliejue/bbafinalproject/MagnetometerActivity.java
+++ app/src/main/java/com/example/kyliejue/bbafinalproject/MagnetometerActivity.java
@@ -10,8 +10,5 @@ public class MagnetometerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_magnetometer);
-
-        Condition testCondition = new Condition(Sensor.TYPE_MAGNETIC_FIELD, "=", "10");
-
     }
 }
